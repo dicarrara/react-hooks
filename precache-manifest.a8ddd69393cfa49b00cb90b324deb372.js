8c5c950618b74bc4261558c72fe7ab03b986ff52
Diana Carrara <dianaVcarrara@gmail.com>
2019-10-15 19:53:20 -0600

    Updates

---
+++ precache-manifest.a8ddd69393cfa49b00cb90b324deb372.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f3d8d5bd97e6c61b1a0cbdcb9bd4196",
+    "url": "/dicarrara/react-hooks.git/index.html"
+  },
+  {
+    "revision": "05d9dd6a343bd6ebd2d5",
+    "url": "/dicarrara/react-hooks.git/static/css/main.35c0f61f.chunk.css"
+  },
+  {
+    "revision": "1b3b3596f8789cc0ad45",
+    "url": "/dicarrara/react-hooks.git/static/js/2.9e86e0bd.chunk.js"
+  },
+  {
+    "revision": "05d9dd6a343bd6ebd2d5",
+    "url": "/dicarrara/react-hooks.git/static/js/main.adcba432.chunk.js"
+  },
+  {
+    "revision": "f58838ae09cf76028eec",
+    "url": "/dicarrara/react-hooks.git/static/js/runtime~main.39d5944a.js"
+  }
+]);
